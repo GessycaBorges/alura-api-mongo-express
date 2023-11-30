bc85f7980f7fecb6fa30585058186818c186b757
Gessyca Borges <gessyca.borges@gmail.com>
2023-11-30 18:50:01 -0300

    Feat: Inserindo rotas

--- src/app.js
+++ src/app.js
@@ -14,17 +14,34 @@ const livros = [
     }
 ];
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
 app.get ("/livros", (req, res) => {
-    res.status(200).json(livros)
+    res.status(200).json(livros);
+});
+
+app.get ("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
 });
 
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso");
 });
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
 
 export default app;
